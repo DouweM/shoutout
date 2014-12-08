ef692d92fbd4ed742ed46240a7fa89fda6aa67af
Håkan Nylén <hakan@fyndiq.se>
2014-12-08 16:22:12 +0100

    using the fix from @DouweM's repo

--- lib/shoutout/stream.rb
+++ lib/shoutout/stream.rb
@@ -38,16 +38,7 @@ module Shoutout
 
       # Read status line
       status_line = @socket.gets
-      print status_line.inspect
-      begin
-        status_code = status_line.match(/\AICY ([0-9]{3})/)[1].to_i
-      rescue NoMethodError
-        begin
-            status_code = status_line.match(/\AHTTP\/([0-9]\.[0-9]) ([0-9]{3})/)[2].to_i
-        rescue NoMethodError
-            status_code = 200
-        end
-      end
+      status_code = status_line.match(/\A(HTTP\/[0-9]\.[0-9]|ICY) ([0-9]{3})/)[2].to_i
 
       @connected = true
 
